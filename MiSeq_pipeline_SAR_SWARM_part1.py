4ced9aee5af76c8fc794ca77ae05f5752f8b9325
Jean-David Grattepanche <jeandavid.grattepanche@gmail.com>
2018-09-14 12:39:02 -0400

    few edits

--- MiSeq_pipeline_SAR_SWARM_part1.py
+++ MiSeq_pipeline_SAR_SWARM_part1.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 
 __author__ = "Jean-David Grattepanche"
-__version__ = "6, June 1st,2018"
+__version__ = "5b, December 19, 2016"
 __email__ = "jeandavid.grattepanche@gmail.com"
 
 
@@ -10,6 +10,7 @@ import re
 import sys
 import os
 from Bio import SeqIO
+from sys import argv
 seqlist = []
 OTUlist = []
 duplicatelist = []
@@ -33,36 +34,31 @@ def runPEAR(folder, file1, file2, path, outputpath, listsample):
 
 
 def main():
-# 	a =sys.argv[1]
- 	b = sys.argv[1]
-
-#	a = input('where your raw data folder is (should be a folder:  /Users/katzlab33/Documents/MiSeq2016/MiSeq_pipeline ) \n ')
-# 	try:
-# 		Path = a
-# 	except ValueError:
-# 		a = ""	
-# 	if a == "":
-# 		print ('Your input is empty.  Try again. ')
-# 	else:
-# 		pathA = a.split(' ')[0]
+	b = sys.argv[1]
 	pathA = os.getcwd()
 	path = pathA + "/Rawdata/"
-#	b = input('where is your sample list file (should be a file:  samplelist.txt: LKM## (tab) samplename ) \n ')
+	listsamp = []
 	try:
 		listsample = b
 	except ValueError:
 		b = ""	
 	if b == "":
 		print ('Your input is empty.  Try again. ')
+	
+	for samp in open(listsample,'r'):
+		if samp.split('\t')[0] not in listsamp:
+			listsamp.append(samp.split('\t')[0])
+	print(len(listsamp), "samples")
 	outputpath = pathA + '/outputs/'
 	if not os.path.exists(outputpath): #+ folder):
 		os.makedirs(outputpath) #+ folder)	
 	temppath = pathA + '/temp/'
 	if not os.path.exists(temppath): #+ folder):
 		os.makedirs(temppath) #+ folder)	
+	filnum = 0
 	for file in os.listdir(path):
 #		print (file)
-		if 'R1' in file:
+		if 'R1' in file and file.split('_')[0] in listsamp:
 			print ("sample: ", file.split('_')[0])
 			print ("file1: ",file, file.split('_')[0])
 			file1 = file
@@ -71,11 +67,16 @@ def main():
 					print ("file2: ",filee)
 					file2 = filee
 					folder = file1.split("_")[0]+"_"+file1.split("_")[1]
-					runPEAR(folder, file1, file2, path, outputpath, listsample)
+					filnum += 1
+					runPEAR(folder, file1, file2, path, outputpath)
+	if len(listsamp) != filnum:
+		print( "ISSUE with sample list! PLEASE CHECK !")
 	else:
-		print (file, "  NOT SEQUENCES FILES")
-		log = open("log.txt",'a')
-		log.write("NOT USED:\t"+ file + '\n')
-		log.close()
+		print("all Okay, go to the part2 !!")
+# 	else:
+# 		print (file, "  NOT SEQUENCES FILES")
+# 		log = open("log.txt",'a')
+# 		log.write("NOT USED:\t"+ file + '\n')
+# 		log.close()
 
 main()
