b1933651b77efcaf10809f67300c6fc844f19833
Jean-David Grattepanche <jeandavid.grattepanche@gmail.com>
2019-03-21 11:19:41 -0400

    clarify parts of the script
    clean and clarify some parts of the script

--- MiSeq_pipeline_SAR_SWARM_part1.py
+++ MiSeq_pipeline_SAR_SWARM_part1.py
@@ -1,3 +1,4 @@
+# python3 MiSeq_pipeline_SAR_SWARM_part1.py list_sample.txt
 #!/usr/bin/python3
 
 __author__ = "Jean-David Grattepanche"
@@ -28,36 +29,35 @@ def runPEAR(folder, file1, file2, path, outputpath):
 		os.system('pear -f ' +  path+file1 + ' -r ' +  path+file2 + ' -o ' + outputpath + 'PEAR/'+ folder  + ' -n 120 -m 300 -q 33 -v 100 -u 0 -j 2')
 		print ("PEAR used the SAR parameters (120/300/33/100/0/2)!! check the parameters!")
 	print ("Convert")
-	if not os.path.exists(outputpath + 'convertPEARfiles/'): #/' + folderP):
-		os.makedirs(outputpath + 'convertPEARfiles/') # + folderP)	
-	os.system('python3 Miseq_scripts/0_convert_fastq_fasta.py ' + outputpath + 'PEAR/' + folderP + '.assembled.fastq') # -c fastq_to_fastaqual -o ' + outputpath + 'convertPEARfiles/') # + folderP) #convert fastq file in fasta + qual files
+	if not os.path.exists(outputpath + 'convertPEARfiles/'): 
+		os.makedirs(outputpath + 'convertPEARfiles/') 	
+	os.system('python3 Miseq_scripts/0_convert_fastq_fasta.py ' + outputpath + 'PEAR/' + folderP + '.assembled.fastq') # -c fastq_to_fastaqual -o ' + outputpath + 'convertPEARfiles/')  #convert fastq file in fasta + qual files
 
 
 def main():
-	b = sys.argv[1]
+	samplefile = sys.argv[1]
 	pathA = os.getcwd()
 	path = pathA + "/Rawdata/"
 	listsamp = []
 	try:
-		listsample = b
+		listsample = samplefile
 	except ValueError:
-		b = ""	
-	if b == "":
-		print ('Your input is empty.  Try again. ')
+		samplefile = ""	
+	if samplefile == "":
+		print ('Your input samplefile is empty.  Try again. ')
 	
 	for samp in open(listsample,'r'):
 		if samp.split('\t')[0] not in listsamp:
 			listsamp.append(samp.split('\t')[0])
 	print(len(listsamp), "samples")
 	outputpath = pathA + '/outputs/'
-	if not os.path.exists(outputpath): #+ folder):
-		os.makedirs(outputpath) #+ folder)	
+	if not os.path.exists(outputpath): 
+		os.makedirs(outputpath) 	
 	temppath = pathA + '/temp/'
-	if not os.path.exists(temppath): #+ folder):
-		os.makedirs(temppath) #+ folder)	
+	if not os.path.exists(temppath): 
+		os.makedirs(temppath) 	
 	filnum = 0
 	for file in os.listdir(path):
-#		print (file)
 		if 'R1' in file and file.split('_')[0] in listsamp:
 			print ("sample: ", file.split('_')[0])
 			print ("file1: ",file, file.split('_')[0])
@@ -73,10 +73,5 @@ def main():
 		print( "ISSUE with sample list! PLEASE CHECK !")
 	else:
 		print("all Okay, go to the part2 !!")
-# 	else:
-# 		print (file, "  NOT SEQUENCES FILES")
-# 		log = open("log.txt",'a')
-# 		log.write("NOT USED:\t"+ file + '\n')
-# 		log.close()
 
 main()
